047be5b6fd6b12bca2abf9e97b3708923b92cc0d
Jacksonckc <jackson858216047@gmail.com>
2022-08-19 16:46:26 -0400

    Finished Animation

--- New Game/src/index.js
+++ New Game/src/index.js
@@ -1,21 +1,101 @@
-import './firebase'
-const canvas = document.getElementById('canvas1')
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const playerImage = new Image();
+playerImage.src = 'src/shadow_dog.png';
 
-const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+const CROPPED_IMG_WiDTH = 575;
+const CROPPED_IMG_HEIGHT = 523;
+let gameFrame = 0;
+const staggerFrames = 10;
 
-const CANVAS_WIDTH = (canvas.width = 600)
-const CANVAS_HEIGHT = (canvas.height = 600)
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', (e) => {
+  playerState = e.target.value;
+});
 
-const playerImage = new Image()
-playerImage.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png'
-let x = 0
+const croppedImageAnimations = [];
+const animationStates = [
+  {
+    name: 'idle',
+    frames: 7,
+  },
+  {
+    name: 'jump',
+    frames: 7,
+  },
+  {
+    name: 'fall',
+    frames: 7,
+  },
+  {
+    name: 'run',
+    frames: 9,
+  },
+  {
+    name: 'dizzy',
+    frames: 11,
+  },
+  {
+    name: 'sit',
+    frames: 5,
+  },
+  {
+    name: 'roll',
+    frames: 7,
+  },
+  {
+    name: 'bite',
+    frames: 7,
+  },
+  {
+    name: 'ko',
+    frames: 12,
+  },
+  {
+    name: 'getHit',
+    frames: 4,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * CROPPED_IMG_WiDTH;
+    let positionY = index * CROPPED_IMG_HEIGHT;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  croppedImageAnimations[state.name] = frames;
+});
+
+console.log(croppedImageAnimations);
 
 function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.fillRect(x, 50, 100, 100)
-  // x++
-  ctx.fillStyle = '#FF0000'
-  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    croppedImageAnimations[playerState].loc.length;
+  let frameX = CROPPED_IMG_WiDTH * position;
+  let frameY = croppedImageAnimations[playerState].loc[position].y;
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    CROPPED_IMG_WiDTH,
+    CROPPED_IMG_HEIGHT,
+    0,
+    0,
+    CROPPED_IMG_WiDTH,
+    CROPPED_IMG_HEIGHT
+  );
+
+  gameFrame++;
+
+  requestAnimationFrame(animate);
 }
 
-animate()
+animate();
