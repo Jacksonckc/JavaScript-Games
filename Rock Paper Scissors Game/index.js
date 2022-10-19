b914289cabcbfe6dc3f52f021a746a1c82d59f28
Jacksonckc <jackson858216047@gmail.com>
2022-10-19 10:29:14 -0600

    1

--- Rock Paper Scissors Game/index.js
+++ Rock Paper Scissors Game/index.js
@@ -1,63 +1,62 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
 
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
   })
-)
+);
 
 function generateComputerChoice() {
-  const randomNum = Math.ceil(Math.random() * 3)
+  const randomNum = Math.ceil(Math.random() * 3);
 
   if (randomNum == 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   } else if (randomNum == 2) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   } else {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
   if (computerChoice == userChoice) {
-    result = 'You are tie!'
+    result = 'You are tie!';
   } else {
     if (computerChoice == 'rock') {
       if (userChoice == 'paper') {
-        result = 'You won!'
+        result = 'You won!';
       } else {
-        result = 'You lost!'
+        result = 'You lost!';
       }
-    } else if (computerChoice == 'Paper') {
+    } else if (computerChoice == 'paper') {
       if (userChoice == 'scissors') {
-        result = 'You won!'
+        result = 'You won!';
       } else {
-        result = 'You lost!'
+        result = 'You lost!';
       }
     } else {
       if (userChoice == 'rock') {
-        result = 'You won!'
+        result = 'You won!';
       } else {
-        result = 'You lost!'
+        result = 'You lost!';
       }
     }
   }
-
-  resultDisplay.innerHTML = result
+  resultDisplay.innerHTML = result;
 }
 
-const h1 = document.querySelector('h1')
+const h1 = document.querySelector('h1');
 gsap.fromTo(
   h1,
   {
@@ -71,4 +70,4 @@ gsap.fromTo(
     duration: 4,
   },
   0.2
-)
+);
